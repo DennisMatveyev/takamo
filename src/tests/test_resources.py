6404ebb4100675ef149ba79d6e3e104a720a3f0f
DennisMatveyev <d.matveev@bigmir.net>
2017-08-04 11:56:36 +0300

    mock tests for rabbit resource

--- src/tests/test_resources.py
+++ src/tests/test_resources.py
@@ -3,35 +3,57 @@ from unittest.mock import MagicMock, patch
 
 from core.config import configuration
 from resources import Connector
+from core.logger import Logger
 
 
 class TestConnector(unittest.TestCase):
+    def setUp(self):
+        self.logger = Logger.get_logger()
+        self.connector = Connector()
+
     def test_get_postgres_conn_success(self):
-        connector = Connector()
         mocked_connect = MagicMock()
         mocked_connect.return_value = 'test'
 
         with patch('psycopg2.connect', mocked_connect):
-            result = connector.get_postgres_conn()
+            result = self.connector.get_postgres_conn()
             mocked_connect.assert_called_once_with(configuration.POSTGRES_LINK)
             self.assertEqual(result, 'test')
 
     def test_get_postgres_conn_failed(self):
-        connector = Connector()
         mocked_connect = MagicMock()
+        mocked_logger = MagicMock()
         mocked_connect.side_effect = Exception('test error')
 
         with patch('psycopg2.connect', mocked_connect):
-            result = connector.get_postgres_conn()
-            mocked_connect.assert_called_once_with(configuration.POSTGRES_LINK)
-            self.assertEqual(result, None)
+            with patch.object(self.logger, 'error', mocked_logger):
+                result = self.connector.get_postgres_conn()
+                mocked_connect.assert_called_once_with(configuration.POSTGRES_LINK)
+                self.assertEqual(result, None)
+                mocked_logger.assert_called_once_with('Unable to connect to the Postgres: test error')
 
     def test_get_rabbit_conn_success(self):
-        connector = Connector()
+        mocked_connect = MagicMock()
+        mocked_params = MagicMock()
+        mocked_connect.return_value = 'test'
 
-        pass
+        with patch('pika.BlockingConnection', mocked_connect):
+            with patch('pika.ConnectionParameters', mocked_params):
+                result = self.connector.get_rabbit_conn()
+
+                mocked_params.assert_called_once_with(host=configuration.RABBIT_MQ_HOST)
+                self.assertEqual(result, 'test')
 
     def test_get_rabbit_conn_failed(self):
-        connector = Connector()
+        mocked_connect = MagicMock()
+        mocked_params = MagicMock()
+        mocked_logger = MagicMock()
+        mocked_connect.side_effect = Exception('test error')
 
-        pass
+        with patch('pika.BlockingConnection', mocked_connect):
+            with patch('pika.ConnectionParameters', mocked_params):
+                with patch.object(self.logger, 'error', mocked_logger):
+                    result = self.connector.get_rabbit_conn()
+                    mocked_params.assert_called_once_with(host=configuration.RABBIT_MQ_HOST)
+                    self.assertEqual(result, None)
+                    mocked_logger.assert_called_once_with('Unable to connect to the Rabbit: test error')
